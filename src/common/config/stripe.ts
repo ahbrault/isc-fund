7300d5ca9309ab83461319a53d826a8edcb57b85
Aurélien Brault <ahbrault@gmail.com>
2025-04-22 10:04:35 +0200

    feat: Change stripe key

--- src/common/config/stripe.ts
+++ src/common/config/stripe.ts
@@ -1,9 +1,5 @@
 import Stripe from 'stripe';
 
-// export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
-export const stripe = new Stripe(
-  'sk_test_51RES5jF0EE8n8UBRhB1ZtJNWXmeP3zcTmKQ168XgcZx7OWYNdzqc8wyLtMzBt2YzawgUUFGhm3pzMZscSCScTJd200JUZq8QMX',
-  {
-    apiVersion: '2025-03-31.basil',
-  }
-);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
+  apiVersion: '2025-03-31.basil',
+});
