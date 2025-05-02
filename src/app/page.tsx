ee527ec769e7d9f0dfb549bc27b035047effbcd8
Aurélien Brault <ahbrault@gmail.com>
2025-05-02 14:14:39 +0200

    feat: Update metadata and add favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,30 +10,6 @@ import {
   WhatIsSickleSection,
 } from './components';
 
-// TODO add metadata
-export const metadata: Metadata = {
-  title: 'International Sickle Cell Fund',
-  description:
-    'ISC Fund fights Sickle Cell Disease by funding life-saving treatment for children in Africa.',
-  openGraph: {
-    title: 'International Sickle Cell Fund',
-    description:
-      'ISC Fund fights Sickle Cell Disease by funding life-saving treatment for children in Africa.',
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
-    type: 'website',
-    images: [
-      {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}/images/kid-1-with-bg.jpg`,
-        width: 1000,
-        height: 1000,
-        alt: 'International Sickle Cell Fund',
-      },
-    ],
-  },
-  // TODO add robots.txt
-  robots: 'index, follow',
-};
-
 const LandingPage: NextPage = () => {
   return (
     <>
