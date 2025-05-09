a22a4b3d7d6285bf5037e25c8461b074c4c88894
Aurélien Brault <ahbrault@gmail.com>
2025-05-09 23:58:24 +0200

    fix: patch admin navigation

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -53,7 +53,7 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
                         key={item.name}
                         href={item.href}
                         className={classNames(
-                          pathname.startsWith(item.startsWith)
+                          pathname?.startsWith(item.startsWith)
                             ? 'border-indigo-500 text-gray-900'
                             : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
                           'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium'
@@ -92,10 +92,10 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
                     as={Link}
                     href={item.href}
                     className={classNames(
-                      pathname.startsWith(item.startsWith)
-                        ? 'border-indigo-500 bg-indigo-50 text-indigo-700'
-                        : 'border-transparent text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800',
-                      'block border-l-4 py-2 pl-3 pr-4 text-base font-medium'
+                      pathname?.startsWith(item.startsWith)
+                        ? 'border-indigo-500 text-gray-900'
+                        : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
+                      'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium'
                     )}
                   >
                     {item.name}
